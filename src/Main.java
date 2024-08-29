3391c1e23b8f5814b57b6f03a70d751000574755
chandra shekhar <chandra.shekhar@knorex.com>
2024-08-29 14:07:17 +0530

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        FunctionalInterfacesExample functionalInterfacesExample = new FunctionalInterfacesExample();
+        functionalInterfacesExample.run();
+
+//        FunctionalProgrammingExample functionalProgrammingExample = new FunctionalProgrammingExample();
+//        functionalProgrammingExample.run();
+
+//        StreamsExample streamsExample = new StreamsExample();
+//        streamsExample.run();
+
+//        StringBuilderExample stringBuilder = new StringBuilderExample();
+//        stringBuilder.run();
+    }
+}
---
+++ src/StreamsExample.java
@@ -0,0 +1,7 @@
+public class StreamsExample {
+    public void run() {
+        // Your streams examples here
+        System.out.println("Streams Examples");
+        // Add your streams-related code here
+    }
+}
